fb65bbb7eb47182169394a91bf8656428c85a8de
BrianvdBerg <brian.vandenberg@student.hu.nl>
2017-09-07 16:31:46 +0200

    Test

---
+++ Les2/pe2_2.py
@@ -0,0 +1,11 @@
+cijferICOR = 5
+cijferPROG = 7
+cijferCSN = 6
+
+cijferLijst = (cijferICOR + cijferPROG + cijferCSN)
+gemiddelde = cijferLijst / 3
+beloning = cijferLijst * 30
+overzicht = 'Mijn cijfers ( met een gemiddelde van een', int(gemiddelde), ') leveren een beloning van ', int(beloning), 'op!'
+
+print(overzicht)
+.
