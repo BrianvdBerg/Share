2288ce27b9e21a38c8ed1c0d58828416c23c2f3a
BrianvdBerg <brian.vandenberg@student.hu.nl>
2017-09-07 16:42:08 +0200

    Test

---
+++ Les2/pe2_4.py
@@ -0,0 +1,9 @@
+uurLoon = eval(input('Zet hier je uurloon: '))
+urenGewerkt = eval(input('Zet hier de uren die je hebt gewerkt: '))
+lijn1 = 'Je uurloon is:', float(uurLoon)
+print(lijn1)
+lijn2 = 'Hoeveelheid uren die je hebt gewerkt:', int(urenGewerkt)
+print(lijn2)
+totaalLoon = urenGewerkt * uurLoon
+lijn3 =  int(urenGewerkt), 'uur werken levert je', int(totaalLoon), 'euro op!'
+print(lijn3)
