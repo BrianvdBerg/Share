9c019a636a08cc034bb564c0101d7bfdff5d2c5a
BrianvdBerg <brian.vandenberg@student.hu.nl>
2017-09-11 11:49:05 +0200

    Test

--- Les3/pe3_3.py
+++ Les3/pe3_3.py
@@ -3,3 +3,6 @@ bezitPaspoort = input('Ben je in bezit van een Nederlands paspoort?: ')
 
 if aantalLeeftijd >= 18 and bezitPaspoort == 'ja':
     print('Gefeliciteerd, je mag stemmen!')
+
+else:
+    print('Helaas, je mag niet stemmen')
---
+++ Les3/pe3_4.py
@@ -0,0 +1,4 @@
+days = ['maandag', 'dinsdag', 'woensdag']
+for day in days:
+    if 'ma' and 'di' and 'wo' in days:
+    print(day)
